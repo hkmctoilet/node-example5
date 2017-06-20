811c7e545acbbc1b31c969935aad995369bb41dd
lkj1197 <lkjljs1197@naver.com>
2017-06-20 13:41:03 +0900

    no message

---
+++ public/script.js
@@ -0,0 +1,25 @@
+	var myApp = angular.module("myApp",["ngRoute"]);
+	myApp.config(function($routeProvider){
+		$routeProvider
+		.when("../public/13F", {
+			templateUrl : "13F.html",
+			controller : "13Fctrl"
+		})
+		.when("../14F", {
+			templateUrl : "14F.html",
+			controller : "14Fctrl"
+		})
+		.when("../15F", {
+			templateUrl : "15F.html",
+			controller : "15Fctrl"
+		});
+	});
+	myApp.controller("13Fctrl", function($scope){
+		$scope.msg = "13층 화장실 가용현황입니다." ;
+	});
+	myApp.controller("14Fctrl", function($scope){
+		$scope.msg = "14층 화장실 가용현황입니다." ;
+	});
+	myApp.controller("15Fctrl", function($scope){
+		$scope.msg = "15층 화장실 가용현황입니다." ;
+	});
